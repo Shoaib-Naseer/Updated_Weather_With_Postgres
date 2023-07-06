da137767f2dd704eaf9e5e76347dac4610ebef01
shoaib naseer <shoaib.naseer.cowlar@gmail.com>
2023-07-06 17:12:38 +0500

    deleted Migrations also

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -1,15 +1,9 @@
-const Weather = require("../models/weather");
+const { queryLatestWeatherData } = require("../influxdb");
 
 exports.getWeather = async (req, res) => {
   try {
     // Fetch the latest weather data from the database
-    const latestWeatherData = await Weather.findOne({
-      where : {
-        city:'Islamabad'
-      },
-      order: [["createdAt", "DESC"]],
-    });
-
+    const latestWeatherData  = await queryLatestWeatherData();
     res.status(200).json(latestWeatherData);
   } catch (error) {
     res.status(500).json({ error: error.message });
